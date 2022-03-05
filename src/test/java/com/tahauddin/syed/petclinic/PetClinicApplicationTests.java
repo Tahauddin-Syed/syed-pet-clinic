83b3f99309d851c4e5f7d14a57d8ff21407b334f
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-03-05 14:47:59 +0300

    Initial commit

---
+++ src/test/java/com/tahauddin/syed/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahauddin.syed.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
