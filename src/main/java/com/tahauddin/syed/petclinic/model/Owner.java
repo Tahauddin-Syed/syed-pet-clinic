fb6f7ac3b74f98828324edbd5b50d992b6bbf726
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-03-05 19:45:28 +0300

    Added POJO to Project

---
+++ src/main/java/com/tahauddin/syed/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.tahauddin.syed.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/tahauddin/syed/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.tahauddin.syed.petclinic.model;
+
+public class Vet extends Person{
+}
