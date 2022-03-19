307c0fb62c80acedb91f8fb90cf0af3f98b154ac
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-03-19 13:08:05 +0300

    Added Entity Annotations on BaseEntity

--- src/main/java/com/tahauddin/syed/petclinic/model/BaseEntity.java
+++ src/main/java/com/tahauddin/syed/petclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package com.tahauddin.syed.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+import static javax.persistence.GenerationType.AUTO;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
 
     public Long getId() {
