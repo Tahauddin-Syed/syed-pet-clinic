6b51b14aadb7fcb5a5670fbb3b678ddd70b43699
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-03-19 15:04:39 +0300

    Added Repository Layer and Interfaces of domain classes

--- src/main/java/com/tahauddin/syed/petclinic/bootstrap/repository/PetRepository.java
+++ src/main/java/com/tahauddin/syed/petclinic/bootstrap/repository/PetRepository.java
@@ -1,4 +1,4 @@
-package com.tahauddin.syed.petclinic.repository;
+package com.tahauddin.syed.petclinic.bootstrap.repository;
 
 import com.tahauddin.syed.petclinic.model.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
