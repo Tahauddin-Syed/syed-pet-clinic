bf93a4e302accf1992593893c59ca19d3c1f0051
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-03-19 13:19:21 +0300

    Added Entity Annotations Domain Classes

--- src/main/java/com/tahauddin/syed/petclinic/model/Pet.java
+++ src/main/java/com/tahauddin/syed/petclinic/model/Pet.java
@@ -1,13 +1,24 @@
 package com.tahauddin.syed.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "PETS")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="pet_type_id")
     private PetType petType;
 
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
+    @Column(name="birth_date")
     private LocalDate birthDate;
 
     public Pet() {
--- src/main/java/com/tahauddin/syed/petclinic/model/PetType.java
+++ src/main/java/com/tahauddin/syed/petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.tahauddin.syed.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PET_TYPE")
 public class PetType extends BaseEntity{
 
+    @Column(name="name")
     private String name;
 
     public PetType() {
