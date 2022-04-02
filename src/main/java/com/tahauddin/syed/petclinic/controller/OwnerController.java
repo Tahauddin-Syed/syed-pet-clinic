7620fa9b7e17ae20db978bb02f1c272e971850ca
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-04-02 13:09:36 +0300

    Added findById in OwnerController and html view for it

--- src/main/java/com/tahauddin/syed/petclinic/controller/OwnerController.java
+++ src/main/java/com/tahauddin/syed/petclinic/controller/OwnerController.java
@@ -1,10 +1,13 @@
 package com.tahauddin.syed.petclinic.controller;
 
+import com.tahauddin.syed.petclinic.model.Owner;
 import com.tahauddin.syed.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping({"/owner", "/owners"})
@@ -24,4 +27,11 @@ public class OwnerController {
         return "Owner/index";
     }
 
+    @GetMapping("/findById/{id}")
+    public ModelAndView findOwnerById(@PathVariable("id") Long ownerId){
+        ModelAndView modelAndView = new ModelAndView("Owner/ownerDetails");
+        Owner ownerById = ownerService.findById(ownerId);
+        modelAndView.addObject(ownerById);
+        return modelAndView;
+    }
 }
---
+++ src/test/java/com/tahauddin/syed/petclinic/service/map/OwnerServiceMapTest.java
@@ -0,0 +1,40 @@
+package com.tahauddin.syed.petclinic.service.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerServiceMapTest {
+
+    OwnerServiceMap ownerServiceMap;
+
+    @BeforeEach
+    void setUp() {
+        ownerServiceMap = new OwnerServiceMap();
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
